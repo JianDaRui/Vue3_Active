923339d39c4fa42441166f139501aed1abe60fc3
JianDaRui <17635269610@163.com>
2021-12-29 20:30:06 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script lang='tsx'>
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+export default defineComponent({
+  components: { HelloWorld },
+  render() {
+    return (
+      <div>
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <HelloWorld/>
+      </div>
+    )
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script lang='tsx'>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup(){
+    return () => (
+      <h1>
+        Hello Vue 3 + JSX
+      </h1>
+    )
+  }
+})
+</script>
